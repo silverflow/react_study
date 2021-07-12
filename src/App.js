684855adee97c268aa7a0686902512d62b83faf2
metafor <silveryflow@naver.com>
2021-07-13 00:09:54 +0900

    study : css 적용(#72p)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import "./App.css";
-import react, { Fragment } from "react";
+import react from "react";
 
 function App() {
 	const name = "리액트";
+
 	return (
-		<>
+		<div className="react">
+			{/*주석은 이렇게 달아야한다!*/}
 			<h1>헬로 {name}</h1>
 			<h2>작동 테스트</h2>
 			<h3>뭔데!</h3>
 			{name === "리액트" ? <h1>리액트입니다.</h1> : <h1>리액트가 아닙니다</h1>}
-		</>
+			<input />
+		</div>
 	);
 }
 
