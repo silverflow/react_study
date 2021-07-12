0365323dc27dea28719d80c99666c129c66f738a
metafor <silveryflow@naver.com>
2021-07-13 02:10:24 +0900

    study: 배열 비구조화 할당한 데이터를 useState를 사용해 상태 변경하기

--- src/Counter.js
+++ src/Counter.js
@@ -6,18 +6,22 @@ export default class Counter extends Component {
 		//state의 초깃값 설정하기
 		this.state = {
 			number: 0,
+			fixNumber: 0,
 		};
 	}
 	render() {
-		const { number } = this.state; // state를 조회할 때는 this.state로 조회한다.
+		const { number, fixNumber } = this.state; // state를 조회할 때는 this.state로 조회한다.
 		return (
 			<div>
 				<h1>{number}</h1>
+				<h2>바뀌지 않는 값: {fixNumber}</h2>
 				<button
 					// onClick을 통해 버튼이 클릭되었을 때 호출할 함수를 지정합니다.
 					onClick={() => {
 						//this.setState를 사용하여 state에 새로운 값을 넣을 수 있습니다.
-						this.setState({ number: number + 1 });
+						this.setState((prevState) => ({
+							number: prevState.number + 1,
+						}));
 					}}
 				>
 					+1
