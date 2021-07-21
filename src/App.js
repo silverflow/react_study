3a10317183329d203989498e1d353d594fdc4ff0
metafor <silveryflow@naver.com>
2021-07-22 04:00:51 +0900

    study: hook의 useEffect 써보기 (#198p)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import React, { Component } from "react";
-import LifeCycleSample from "./LifeCycleSample";
-import ErrorBoundary from "./ErrorBoundary";
-// 랜덤 색상 선택
-function getRandomColor() {
-	return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-export default class App extends Component {
-	state = {
-		color: "#000000",
-	};
-	handleClick = () => {
-		this.setState({
-			color: getRandomColor(),
-		});
-	};
-	render() {
-		return (
-			<div>
-				<button onClick={this.handleClick}>랜덤 색상</button>
-				<ErrorBoundary>
-					<LifeCycleSample color={this.state.color} />
-				</ErrorBoundary>
-			</div>
-		);
-	}
-}
+import React, { useState } from "react";
+import Info from "./hooks/Info";
+
+const App = () => {
+	const [visible, setVisible] = useState(false);
+	return (
+		<div>
+			<button
+				onClick={() => {
+					setVisible(!visible);
+				}}
+			>
+				{visible ? "숨기기" : "보이기"}
+			</button>
+			<hr />
+			{visible && <Info />}
+		</div>
+	);
+};
+
+export default App;
