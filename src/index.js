fb4f32260d3c3572f12682b305e9deb1df4bcd9c
metafor <silveryflow@naver.com>
2021-08-03 00:06:46 +0900

    fix: immer를 사용해서 불변성 유지하기

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./style/App";
+import App from "./12_immer/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
