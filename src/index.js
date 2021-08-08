8a4dc2d04ad55c6514a123270d8472d13058f743
metafor <silveryflow@naver.com>
2021-08-08 21:43:05 +0900

    study: 외부 api 연동해서 axios를 통해서 news viewer 만들기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
+import App from "./14_news_viewer/App";
 import "./index.css";
-import App from "./13_router/App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
