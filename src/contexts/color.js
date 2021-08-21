39d11c5da76b32e1b2a057b2374b72d9ad6d3dbf
metafor <silveryflow@naver.com>
2021-08-21 19:21:29 +0900

    study: consumer 대신 hook 도는 static contextType 사용하기

--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,5 +1,15 @@
-import { createContext } from "react";
+import React, { createContext, useState } from "react";
 
-const ColorContext = createContext({ color: "black" });
+const ColorContext = createContext({
+	state: { color: "black", subcolor: "red" },
+	actions: {
+		setColor: () => {},
+		setSubcolor: () => {},
+	},
+});
+
+const ColorProvider = ({ children }) => {
+	const [color, setColor] = useState("black");
+};
 
 export default ColorContext;
